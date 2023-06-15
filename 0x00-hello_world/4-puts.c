51c187038c2f17aa5ca37751ef20e21b7e7ce270
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-16 02:20:42 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+* main -entry point
+*
+*description: A C program that prints with pu function
+*
+*return: always 0 (success)
 int main(void) 
 {
 	puts ("\"Programming is like building a multilingual puzzle");
