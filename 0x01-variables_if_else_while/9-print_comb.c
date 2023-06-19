76b84ee8a6a8c3be5a3ca67a6d2f2363b04f8590
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-20 00:21:30 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 int d;
-for (d = '0'; d >= '9'; d++)
+for (d = '0'; d <= '9'; d++)
 {
 putchar(d);
-if (d != '9' )
+if (d != '9')
 {
 putchar(',');
 putchar(' ');
@@ -19,4 +19,4 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
-} 
+}
