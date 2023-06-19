c8632d840afc5e880618f9057e18c9b384b4a897
yassminassem <yassminassem@gmail.com>
2023-06-19 13:27:11 -0700

    fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main- prints the alphabetic
+*Return: Always (success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+ } 
