7495254421b24aba924057fc2f4cd74d34f5d9d3
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-21 02:19:49 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _abs - computers the absolute value of an integer.
+* @c: the number to be computed.
+* Return: absolute value of number or zero
+*/
+int _abs(int c)
+{
+if (c< 0)
+{
+int abs_val;
+abs_val =  * -1;
+return (abs_val);
+}
+return (c);
+}
