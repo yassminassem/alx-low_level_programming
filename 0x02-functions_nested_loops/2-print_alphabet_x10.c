043a69ae34a7d11d7ef13b625a7885ab419a9586
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-20 23:47:36 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,13 @@
 *print_alphabet_x10 - make alphabet x10 times
 * Return: void
 */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 char c;
 int i = 0;
 while (i <= 9)
 {
-for ( c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
