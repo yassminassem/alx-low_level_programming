601aac2181ab5aa008765ddbb9c2eb91390c8b66
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-21 01:57:23 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,19 @@
 */
 int Print_sign(int n)
 {
-if ( n > 0)
+if (n > 0)
 {
 _putchar(43);
 return (1);
 }
-else if (n <0)
+else if ( n == 0)
 {
-_putchar(45);
-return (-1);
+_putchar(48);
+return (0);
 }
 else
 {
-_putchar(48);
-return(0);
+_putchar(45);
+return (-1);
 }
 }
