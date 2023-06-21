98a881c40f823f5623cb3714f9d5b42672bfda9f
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-21 03:39:31 +0300

    Create 10-add.c

---
+++ 10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - Adds two values
+* @n1: the first number to be computed
+* @n2: the second number to be computed
+* Return: intger value
+*/
+int add(int n1, int n2)
+{
+return (n1 +n2);
+}
