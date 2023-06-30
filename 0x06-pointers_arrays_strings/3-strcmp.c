cf8b9eaeebacc88780801fa3cc0745e903b59bd7
yassminassem <yassminassem@gmail.com>
2023-06-29 18:56:44 -0700

    fix

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+	char s1[] = "hello";
+char s2[] = "world";
+printf("%d\n", _strcmp(s1, s2));
+printf("%d\n", _strcmp(s2, s1));
+printf("%d\n", _strcmp(s1, s1));
+return (0);
+}
