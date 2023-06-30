b3fafc3b928506940bf9a080adadabbc47548366
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-06-30 04:45:31 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,17 +4,21 @@
 *
 * @dest: pointer to destnation input
 * @src: pointer to source input
+*
 *Return: pointer to resulting string @dest
 */
+
 char *_strcat(char *dest, char *src);
 {
-int c, c2;
+    int c, c2;
 
-c = 0;
+    c = 0;
 
-while (dest[c])
-c++,
-for (c2 = 0; src[c2] ; c2++) 
-dest[c++] = src[c2];
-return (dest);
+    while (dest[c])
+     c++;
+  
+     for (c2 = 0; src[c2] ; c2++) 
+    dest[c++] = src[c2];
+  
+   return (dest);
 }
