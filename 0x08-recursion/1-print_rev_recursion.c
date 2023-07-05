55203e174937791937d8fd9b8862a415498223f7
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-07-05 18:56:19 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,9 +5,9 @@
  * Return: void
  */
 void _print_rev_recursion(char *s)
-{
+{        
         if (*s > '\0')
-        {
+{
                 _print_rev_recursion(s + 1);
                 _putchar(*s);
  }
