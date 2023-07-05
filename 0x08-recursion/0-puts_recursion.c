bec87c8d7b4d18f9d2654db93d18f272c6ef9282
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-07-05 18:47:15 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,14 @@
  * Return: void
  */
 void _puts_recursion(char *s)
-{ 
-        if(*s == '\0')
+{
+        if (*s == '\0')
         {
                 _putchar('\n');
         }
         else
         {
-                _putchar(*s);
+                  _putchar(*s);
                  _puts_recursion(s + 1);
         }
 }
