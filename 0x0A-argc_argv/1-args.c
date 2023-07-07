5afd22ea9813cccc1e2f488ece88065a724e9cf7
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-07-07 22:37:23 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - print number of arguments passed.
+*@argc: number of commandline arguments.
+*@argv: pointer to an array of commandline arguments.
+*Return: 0-success, non-zero-fail.
+*/
+int main(int argc, char *argv[]  __attribute__((unsend)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
