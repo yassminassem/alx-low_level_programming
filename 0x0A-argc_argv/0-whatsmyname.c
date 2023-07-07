52aebfde6f8f971a3f6c663c9f94c3cf5e02d025
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-07-07 22:25:41 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
 * main - prints its name 
@@ -8,7 +7,7 @@
 */
 int main(int argc, char *argv[])
 {
-(void) argc;
+(void)argc;
 priintf("%s\n", argv[0]);
 return(0);
 }
