512c463c1a5cbb0ddcedb41a9db9ef939ebcbeb0
yassminassem <133427460+yassminassem@users.noreply.github.com>
2023-07-08 17:32:16 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,36 @@
 #include "main.h"
+
+void print_number(int num);
+
 /**
-*main - print number of arguments passed.
-*@argc: number of commandline arguments.
-*@argv: pointer to an array of commandline arguments.
-*Return: 0-success, non-zero-fail.
-*/
-int main(int argc, char *argv[]  __attribute__((unsend)))
+ * main - Prints the number of arguments passed into it
+ *
+ * @argc: Length of @argv
+ *
+ * @argv: Array of strings of the programs arguments
+ *
+ * Return: 0, Success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d\n", argc - 1);
-return (0);
+	print_number(--argc);
+	_putchar('\n');
+	return (0);
+}
+
+/**
+ * print_number - prints number with _putchar
+ *
+ * @num: inter, number to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int num)
+{
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
 }
